70ae02d5bceee325e9107c7b850f981af75f6c1b
Sárándi <sarandi.balint@gmail.com>
2022-12-13 14:06:48 +0100

    dog-view fxml, dogcontroller, delete method

--- src/main/java/hu/petrik/adatbazisgui/DogController.java
+++ src/main/java/hu/petrik/adatbazisgui/DogController.java
@@ -1,14 +1,145 @@
 package hu.petrik.adatbazisgui;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
 public class DogController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TableView<Dog> dogTable;
+    @FXML
+    private TableColumn<Dog, String> nameCol;
+    @FXML
+    private TableColumn<Dog, Integer> ageCol;
+    @FXML
+    private TableColumn<Dog, String> breedCol;
+    private DogDB db;
+    @FXML
+    private TextField nameInput;
+    @FXML
+    private TextField breedInput;
+    @FXML
+    private Spinner<Integer> ageInput;
+    @FXML
+    private Button updateButton;
+    @FXML
+    private Button deleteButton;
+    @FXML
+    private Button submitButton;
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private void initialize() {
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        ageCol.setCellValueFactory(new PropertyValueFactory<>("age"));
+        breedCol.setCellValueFactory(new PropertyValueFactory<>("breed"));
+        ageInput.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 50));
+        try {
+            db = new DogDB();
+            readDogs();
+        } catch (SQLException e) {
+            Platform.runLater(() -> {
+                sqlAlert(e);
+                Platform.exit();
+            });
+        }
+    }
+
+    private void sqlAlert(SQLException e) {
+        alert(Alert.AlertType.ERROR,
+                "Hiba történt az adatbázis kapcsolat kialakításakor",
+                e.getMessage());
+    }
+
+    private void readDogs() throws SQLException {
+        List<Dog> dogs = db.readDogs();
+        dogTable.getItems().clear();
+        dogTable.getItems().addAll(dogs);
+    }
+
+    private Optional<ButtonType> alert(Alert.AlertType alertType, String headerText, String contentText) {
+        Alert alert = new Alert(alertType);
+        alert.setHeaderText(headerText);
+        alert.setContentText(contentText);
+        return alert.showAndWait();
+    }
+
+    @FXML
+    public void updateClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void deleteClick(ActionEvent actionEvent) {
+        int selectedIndex = dogTable.getSelectionModel().getSelectedIndex();
+        if (selectedIndex == -1) {
+            alert(Alert.AlertType.WARNING,
+                    "Törléshez előbb válasszon ki kutyát a táblázatból", "");
+            return;
+        }
+        Optional<ButtonType> optionalButtonType = alert(Alert.AlertType.CONFIRMATION,
+                "Biztos, hogy törölni szeretné a viláasztott kutyát?", "");
+        if (optionalButtonType.isEmpty() ||
+                (!optionalButtonType.get().equals(ButtonType.OK) &&
+                        !optionalButtonType.get().equals(ButtonType.YES))) {
+            return;
+        }
+        Dog selected = dogTable.getSelectionModel().getSelectedItem();
+
+        try {
+            if (db.deleteDog(selected.getId())) {
+                alert(Alert.AlertType.WARNING, "Sikeres törlés", "");
+            } else {
+                alert(Alert.AlertType.WARNING, "Sikertelen törlés", "");
+            }
+            readDogs();
+        } catch (SQLException e) {
+            sqlAlert(e);
+        }
+    }
+
+    @FXML
+    public void submitClick(ActionEvent actionEvent) {
+        String name = nameInput.getText().trim();
+        int age = ageInput.getValue();
+        String breed = breedInput.getText().trim();
+        if (name.isEmpty()) {
+            alert(Alert.AlertType.WARNING, "Név megadása kötelező", "");
+            return;
+        }
+        if (breed.isEmpty()) {
+            alert(Alert.AlertType.WARNING, "Fajta megadása kötelező", "");
+            return;
+        }
+        Dog dog = new Dog(name, age, breed);
+        try {
+            if (db.createDog(dog)) {
+                alert(Alert.AlertType.WARNING, "Sikeres felvétel", "");
+                resetForm();
+                readDogs();
+            } else {
+                alert(Alert.AlertType.WARNING, "Sikertelen felvétel", "");
+            }
+        } catch (SQLException e) {
+            sqlAlert(e);
+        }
+
+    }
+
+    private void resetForm() {
+        nameInput.setText("");
+        ageInput.getValueFactory().setValue(0);
+        breedInput.setText("");
+    }
+
+    @FXML
+    public void cancelClick(ActionEvent actionEvent) {
     }
 }
--- src/main/java/hu/petrik/adatbazisgui/DogDB.java
+++ src/main/java/hu/petrik/adatbazisgui/DogDB.java
@@ -1,6 +1,8 @@
 package hu.petrik.adatbazisgui;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DogDB {
     Connection conn;
@@ -16,19 +18,39 @@ public class DogDB {
         conn = DriverManager.getConnection(url,DB_USERNAME,DB_PASSWORD);
     }
 
-    public void createDog() {
-
+    public boolean createDog(Dog dog) throws SQLException {
+        String sql = "INSERT INTO dogs(name, age, breed) VALUES (?, ?, ?)";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setString(1, dog.getName());
+        stmt.setInt(2, dog.getAge());
+        stmt.setString(3, dog.getBreed());
+        return stmt.executeUpdate() > 0;
     }
 
-    public void readDogs() {
-
+    public List<Dog> readDogs() throws SQLException{
+        List<Dog> dogs = new ArrayList<>();
+        String sql = "SELECT * FROM dogs";
+        Statement stmt = conn.createStatement();
+        ResultSet result = stmt.executeQuery(sql);
+        while (result.next()) {
+            int id = result.getInt("id");
+            String name = result.getString("name");
+            int age = result.getInt("age");
+            String breed = result.getString("breed");
+            Dog dog = new Dog(id, name, age, breed);
+            dogs.add(dog);
+        }
+        return dogs;
     }
 
     public void updateDog() {
 
     }
 
-    public void deleteDog() {
-
+    public boolean deleteDog(int id) throws SQLException{
+        String sql = "DELETE FROM dogs WHERE id = ?";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setInt(1, id);
+        return stmt.executeUpdate() > 0;
     }
 }
